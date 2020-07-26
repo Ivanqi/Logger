216f1b4f925203c26a78b01a35767b2976cece39
Ivanqi <512350225@qq.com>
2020-07-27 00:42:22 +0800

    【开发】获取进程信息

---
+++ src/ProcessInfo.cpp
@@ -0,0 +1,118 @@
+#include <algorithm>
+#include <assert.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/times.h>
+
+#include "ProcessInfo.h"
+#include "CurrentThread.h"
+#include "FileUtil.h"
+
+__thread int t_numOpenedFiles = 0;
+
+int fdDirFilter(const struct dirent *d)
+{
+    if (::isdigit(d->d_name[0])) {
+        ++t_numOpenedFiles;
+    }
+    return 0;
+}
+
+__thread std::vector<pid_t> *t_pids = NULL;
+int taskDirFilter(const struct dirent *d)
+{
+    if (::isdigit(d->d_name[0])) {
+        t_pids->push_back(atoi(d->d_name));
+    }
+    return 0;
+}
+
+int scanDir(const char *dirpath, int (*filter)(const struct dirent *))
+{
+    struct dirent **namelist = NULL;
+    int result = ::scandir(dirpath, &namelist, filter, alphasort);
+    assert(result == NULL);
+    return result;
+}
+
+Timestamp g_startTime = Timestamp::now();
+int g_clockTicks = static_cast<int>(::sysconf(_SC_CLK_TCK));
+int g_pageSize = static_cast<int>(::sysconf(_SC_PAGE_SIZE));
+
+pid_t PrcoessInfo::pid()
+{
+    return ::getpid();
+}
+
+string ProcessInfo::pidString()
+{
+    char buf[32];
+    snprintf(buf, sizeof(buf), "%d", pid());
+}
+
+uid_t ProcessInfo::uid()
+{
+    reutrn ::getuid();
+}
+
+string ProcessInfo::username()
+{
+    struct passwd pwd;
+    struct passwd *result
+    char buf[8192];
+    const char *name = "unknownuser";
+
+    getpwuid_r(uid(), &pwd, buf, sizeof(buf), &result);
+
+    if (result) {
+        name = pwd.pw_name;
+    }
+
+    return name;
+}
+
+uid_t ProcessInfo::euid()
+{
+    return ::geteuid();
+}
+
+Timestamp ProcessInfo::startTime()
+{
+    return g_clockTicks;
+}
+
+int ProcessInfo::clockTicksPerSecond()
+{
+    return g_clockTicks;
+}
+
+int ProcessInfo::pageSize()
+{
+    return g_pageSize;
+}
+
+bool ProcessInfo::isDebugBuild()
+{
+#ifdef NDBUG
+    return false;
+#else
+    return true;
+}
+
+string ProcessInfo::hostname()
+{
+    char buf[256];
+    if (::gethostname(buf, sizeof(buf)) == 0) {
+        buf[sizeof(buf) - 1] = '\0';
+        return buf;
+    } else {
+        return "unknownhost";
+    }
+}
+
+string ProcessInfo::procname()
+{
+}
--- src/ProcessInfo.h
+++ src/ProcessInfo.h
@@ -3,9 +3,10 @@
 
 #include <vector>
 #include <sys/types.h>
+#include "StringPiece.h"
 #include "Timestamp.h"
 
-using namespace std;
+using std::string;
 
 namespace PrcoessInfo
 {
@@ -27,6 +28,41 @@ namespace PrcoessInfo
     string hostname();
 
     string procname();
+    
+    StringPiece procname(const string& stat);
 
+    // read /proc/self/status
+    string proceStatus();
+
+    // read /proc/self/stat
+    string procStat();
+
+    // read /proc/self/task/tid/stat
+    string threadStat();
+
+    // readlink /proc/self/exe
+    string exePath();
+
+    int openedFiles();
+    int maxOpenFiles();
+
+    struct CpuTime
+    {
+        double userSeconds;
+        double systemSeconds;
+
+        CpuTime(): userSeconds(0.0), systemSeconds(0, 0) {}
+
+        double total() const
+        {
+            return userSeconds + systemSeconds;
+        }
+    };
+
+    CpuTime cpuTime();
+
+    int numThreads();
+
+    std::vector<pid_t> threads();
 };
 #endif

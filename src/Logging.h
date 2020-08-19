6a299a9235714d4e0908580ac700ce2ff4f20e6e
Ivanqi <512350225@qq.com>
2020-08-19 18:18:02 +0800

    【开发】增加测试案例

--- src/Logging.h
+++ src/Logging.h
@@ -119,5 +119,21 @@ const char *strerror_tl(int savedErrno);
 #define LOG_INFO if (Logger::logLevel() <= Logger::INFO)    \
     Logger(__FILE__, __LINE__).stream();
 
+#define LOG_DEBUG if (Logger::logLevel() <= Logger::DEBUG) \
+    Logger(__FILE__, __LINE__, Logger::DEBUG, __func__).stream();
+
+#define LOG_TRACE if (Logger::logLevel() <= Logger::TRACE) \
+    Logger(__FILE__, __LINE__, Logger::TRACE, __func__).stream();
+
+#define LOG_WARN Logger(__FILE__, __LINE__, ogger::WARN).stream()
+
+#define LOG_ERROR Logger(__FILE__, __LINE__, Logger::ERROR).stream()
+
+#define LOG_FATAL Logger(__FILE__, __LINE__, Logger::FATAL).stream()
+
+#define LOG_SYSERR Logger(__FILE__, __LINE__, false).stream()
+
+#define LOG_SYSFATAL Logger(__FILE__, __LINE__, true).stream()
+
 
 #endif

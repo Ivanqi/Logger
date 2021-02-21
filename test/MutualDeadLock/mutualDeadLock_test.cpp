9605a96efdcb1eb57c83396f28b30e3b8000edf9
Ivanqi <512350225@qq.com>
2021-02-21 23:57:18 +0800

    【开发】修改代码

--- test/MutualDeadLock/mutualDeadLock_test.cpp
+++ test/MutualDeadLock/mutualDeadLock_test.cpp
@@ -7,97 +7,76 @@ class Request;
 
 class Inventory
 {
- public:
-  void add(Request* req)
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    requests_.insert(req);
-  }
+    private:
+        mutable MutexLock mutex_;
+        std::set<Request*> request_;
+    
+    public:
+        void add(Request *req)
+        {
+            MutexLockGuard lock(mutex_);
+            request_.insert(req);
+        }
 
-  void remove(Request* req) __attribute__ ((noinline))
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    requests_.erase(req);
-  }
+        void remove(Request *req)
+        {
+            MutexLockGuard lock(mutex_);
+            request_.erase(req);
+        }
 
-  void printAll() const;
-
- private:
-  mutable muduo::MutexLock mutex_;
-  std::set<Request*> requests_;
+        void printAll() const;
 };
 
 Inventory g_inventory;
 
 class Request
 {
- public:
-  void process() // __attribute__ ((noinline))
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    g_inventory.add(this);
-    // ...
-  }
+    private:
+        mutable MutexLock mutex_;
+    
+    public:
+        void process()
+        {
+            MutexLockGuard lock(mutex_);
+            g_inventory.add(this);
+        }
 
-  ~Request() __attribute__ ((noinline))
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    sleep(1);
-    g_inventory.remove(this);
-  }
+        ~Request()
+        {
+            MutexLockGuard lock(mutex_);
+            sleep(1);
+            g_inventory.remove(this);
+        }
 
-  void print() const __attribute__ ((noinline))
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    // ...
-  }
-
- private:
-  mutable muduo::MutexLock mutex_;
+        void print()
+        {
+            MutexLockGuard lock(mutex_);
+            // ...
+        }
 };
 
 void Inventory::printAll() const
 {
-  muduo::MutexLockGuard lock(mutex_);
-  sleep(1);
-  for (std::set<Request*>::const_iterator it = requests_.begin();
-      it != requests_.end();
-      ++it)
-  {
-    (*it)->print();
-  }
-  printf("Inventory::printAll() unlocked\n");
+    MutexLockGuard lock(mutex_);
+    sleep(1);
+
+    for (std::set<Request*>::const_iterator it = request_.begin(); it != request_.end(); ++it) {
+        (*it)->print();
+    }
+
+    printf("Inventory::printAll() unlocked\n");
 }
 
-/*
-void Inventory::printAll() const
-{
-  std::set<Request*> requests
-  {
-    muduo::MutexLockGuard lock(mutex_);
-    requests = requests_;
-  }
-  for (std::set<Request*>::const_iterator it = requests.begin();
-      it != requests.end();
-      ++it)
-  {
-    (*it)->print();
-  }
-}
-*/
-
-void threadFunc()
-{
-  Request* req = new Request;
-  req->process();
-  delete req;
+void threadFunc() {
+    Request *req = new Request;
+    req->process();
+    delete req;
 }
 
-int main()
-{
-  muduo::Thread thread(threadFunc);
-  thread.start();
-  usleep(500 * 1000);
-  g_inventory.printAll();
-  thread.join();
+int main() {
+    Thread th(threadFunc);
+    th.start();
+    usleep(500 * 1000);
+    g_inventory.printAll();
+    th.join();
 }
